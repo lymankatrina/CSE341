223ebb9a47fb2e942f30284fc33c9381dd993544
Katrina Lyman <lymankatrina@gmail.com>
2024-01-20 23:36:01 -0700

    week2

---
+++ routes/contacts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const contactsController = require("../controllers/contacts");
+
+const router = express.Router();
+
+router.get('/', contactsController.getData);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 // This is going to create a modular web application using node.js express routers
 const express = require("express");
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+const contactsRoutes = require('./routes/contacts');
 
-app.listen(port, () => {
-	console.log(`Web Server is listening on port ${port}`);
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/contacts', contactsRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Connected to DB and Web Server is listening on port ${port}`);
+    });
+
+  }
 });
+
+
